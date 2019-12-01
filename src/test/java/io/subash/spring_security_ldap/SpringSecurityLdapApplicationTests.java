0ceab5057026cddf78351a810749d949ecfb8be2
subash kharel <subash-minu@localhost.local>
2019-11-30 20:49:01 -0600

    first commit

---
+++ src/test/java/io/subash/spring_security_ldap/SpringSecurityLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package io.subash.spring_security_ldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
